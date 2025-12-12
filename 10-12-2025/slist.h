ecc7f4477026739a01321adf5b3cd0fc5fe51798
anosh <anosh2604@gmail.com>
2025-12-12 21:33:51 +0530

    Removed the global declaration of s list and the null defination which was unnecessary

--- 10-12-2025/slist.h
+++ 10-12-2025/slist.h
@@ -1,6 +1,5 @@
 #ifndef SLIST_H
 #define SLIST_H
-#define NULL ((void *)0)
 typedef int listElementType;
 typedef struct node{
     listElementType info;
@@ -9,11 +8,10 @@ typedef struct node{
 
 typedef nodeType* nodePtrType;
 typedef nodePtrType sListType;
-sListType sList;
 sListType create_sList();
 sListType destroy_sList(sListType sList);
 sListType insertBeginSList(sListType sList,listElementType newElement);
-sListType insertEndinSList(sListType sList,listElementType newElement);
+sListType insertEndSList(sListType sList,listElementType newElement);
 sListType insertPosInSList(sListType sList,listElementType newElement,int pos);
 void displayList(sListType sList);
 listElementType retrieve_ithElement_sList(sListType sList,int pos);
