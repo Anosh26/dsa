cfa97d123a76bf2cdcc832caf94db62a63ce5728
anosh <anosh2604@gmail.com>
2025-12-13 23:07:02 +0530

    Implements singly linked list application
    Implements a menu-driven application to manage a singly linked list.
    
    Adds functionality for inserting, deleting, retrieving, and displaying elements.
    Includes error handling for invalid input and empty list scenarios.

--- 10-12-2025/slistApp.c
+++ 10-12-2025/slistApp.c
@@ -1,18 +1,109 @@
-#include<slist.h>
-#include<stdio.h>
-#include<stdlib.h>
-void printMenu(){
+#include <stdio.h>
+#include <stdlib.h>
+#include "slist.h"
+
+void printMenu() {
     printf("\n--- Singly Linked List Manager ---\n");
-    printf("1.Insert At the Beginning\n");
-    printf("2.Insert At the End\n");
-    printf("3.Insert At the Position\n");
-    printf("4.Delete At the Beginning\n");
-    printf("5.Retrieve the Ith Element\n");
-    printf("6. At the Position\n");
-    printf("7.Insert At the Position\n");
+    printf("1. Insert at Beginning\n");
+    printf("2. Insert at End\n");
+    printf("3. Insert at Position\n");
+    printf("4. Delete from Beginning\n");
+    printf("5. Delete from End\n");       
+    printf("6. Delete from Position\n");  
+    printf("7. Retrieve Element\n");
+    printf("8. Display List\n");
+    printf("0. Exit\n");
+    printf("Enter choice: ");
 }
-int main(){
 
-    create_sList();
+int main() {
+    sListType myList = create_sList();
+    
+    int choice, value, pos;
+    listElementType result;
 
+    while(1) {
+        printMenu();
+        if (scanf("%d", &choice) != 1) {
+            while(getchar() != '\n'); 
+            continue;
+        }
+
+        switch(choice) {
+            case 1: // Insert Begin
+                printf("Enter integer to insert: ");
+                scanf("%d", &value);
+                myList = insertBeginSList(myList, value);
+                printf("Inserted %d at beginning.\n", value);
+                break;
+
+            case 2: // Insert End
+                printf("Enter integer to insert: ");
+                scanf("%d", &value);
+                myList = insertEndinSList(myList, value);
+                printf("Inserted %d at end.\n", value);
+                break;
+
+            case 3: // Insert Position
+                printf("Enter integer to insert: ");
+                scanf("%d", &value);
+                printf("Enter position (0-based): ");
+                scanf("%d", &pos);
+                myList = insertPosInSList(myList, value, pos);
+                break;
+
+            case 4: // Delete Begin
+                if (myList == NULL) {
+                    printf("List is already empty.\n");
+                } else {
+                    myList = deleteBeginSList(myList);
+                    printf("Deleted first element.\n");
+                }
+                break;
+
+            case 5: // Delete End 
+                if (myList == NULL) {
+                    printf("List is already empty.\n");
+                } else {
+                    myList = deleteEndSList(myList);
+                    printf("Deleted last element.\n");
+                }
+                break;
+
+            case 6: // Delete Position 
+                if (myList == NULL) {
+                    printf("List is empty.\n");
+                } else {
+                    printf("Enter position to delete (0-based): ");
+                    scanf("%d", &pos);
+                    myList = deletePosinSList(myList, pos);
+                    printf("Deletion operation complete.\n");
+                }
+                break;
+
+            case 7: // Retrieve
+                printf("Enter position to retrieve: ");
+                scanf("%d", &pos);
+                result = retrieve_ithElement_sList(myList, pos);
+                if (result != -1) { 
+                    printf("Element at %d is: %d\n", pos, result);
+                }
+                break;
+
+            case 8: // Display
+                printf("Current List: ");
+                displayList(myList);
+                break;
+
+            case 0: // Exit
+                printf("Cleaning up memory...\n");
+                myList = destroy_sList(myList);
+                printf("Exiting. Goodbye!\n");
+                return 0;
+
+            default:
+                printf("Invalid choice. Try again.\n");
+        }
+    }
+    return 0;
 }
